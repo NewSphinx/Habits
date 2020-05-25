5547fbbbda9d6898345b2b0045a72d47e6f31d9f
NewSphinx <newersphinx@gmail.com>
2020-05-25 19:58:48 +0530

    Fresh start

--- src/router/index.js
+++ src/router/index.js
@@ -10,29 +10,6 @@ const routes = [
     name: 'Home',
     component: Home,
   },
-  {
-    path: '/item/:name',
-    name: 'Item',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Item.vue'),
-  },
-  {
-    path: '/settings',
-    name: 'Settings',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Settings.vue'),
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-  {
-    path: '/faq',
-    name: 'Faq',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Faq.vue'),
-  },
 ];
 
 const router = new VueRouter({
